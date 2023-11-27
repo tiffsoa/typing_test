5ade95e7659fac67c3b9913954bc8d70b879b206
tiffsoa <atiff.soa@gmail.com>
2023-11-26 19:35:41 -0500

    timer and char

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
-# This is a sample Python script.
+import time
+import datetime
+import sys, termios, tty
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
 def main():
+    timer(1)
+    getch()
     print("Hi")
+
+
+def timer(minutes: int):
+    total_seconds = minutes * 60
+    while total_seconds > 0:
+        countdown = datetime.timedelta(seconds=total_seconds)
+        print(countdown, end="\r")
+        time.sleep(1)
+        total_seconds -= 1
+    print("Stop! 0 seconds left!")
+
+
+def getch():
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
 
 
 if __name__ == '__main__':
